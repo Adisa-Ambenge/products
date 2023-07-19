54ef101bff3b89bf910f78ea12c6cfea55466414
Adisa-Ambenge <ambengeangela@gmail.com>
2023-07-19 14:27:02 +0300

    products display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './Login';
+import Home from './Home';
+import Navigation from './Navigation';
+import ProductDetails from './Products';
+import NewProductForm from './NewProduct';
+
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation/>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/Login" element={<Login/>}></Route>
+      <Route path="/Home" element={<Home/>}></Route>
+      <Route  path="/Products" element={<ProductDetails/>}/>
+      <Route path="/products/new" element={<NewProductForm/>} />
+
+     </Routes>
+     </BrowserRouter>
+      
     </div>
   );
 }
 
 export default App;
+
